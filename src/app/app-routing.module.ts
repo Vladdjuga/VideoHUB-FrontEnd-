0134289b1bed83fee3795375d2652cc6f1d352d1
Артем Кулiк <arti_vvl@rivne.itstep.org>
2021-07-19 15:48:16 +0300

    easy fon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: '', component: IndexComponent }
 ];
 
 @NgModule({
