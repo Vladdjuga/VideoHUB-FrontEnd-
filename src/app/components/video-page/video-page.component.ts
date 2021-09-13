fed1db389ce86ebed8f1d162afe70fd32f14ccdc
Anastasia-Troian <nasara.my.ten@gmail.com>
2021-09-13 16:07:41 +0300

    Update Recommendation

--- src/app/components/video-page/video-page.component.ts
+++ src/app/components/video-page/video-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Video } from 'src/app/models/video';
 import { VideoService } from 'src/app/services/video.service';
@@ -8,118 +8,6 @@ import {FlatTreeControl} from '@angular/cdk/tree';
 import {BehaviorSubject, merge, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import { User } from 'src/app/models/user';
-
-/** Flat node with expandable and level information */
-export class DynamicFlatNode {
-  constructor(public item: string, public level = 1, public expandable = false,
-              public isLoading = false) {}
-}
-
-/**
- * Database for dynamic data. When expanding a node in the tree, the data source will need to fetch
- * the descendants data from the database.
- */
-@Injectable({providedIn: 'root'})
-export class DynamicDatabase {
-  dataMap = new Map<string, string[]>([
-    ['Fruits', ['Apple', 'Orange', 'Banana']],
-    ['Vegetables', ['Tomato', 'Potato', 'Onion']],
-    ['Apple', ['Fuji', 'Macintosh']],
-    ['Onion', ['Yellow', 'White', 'Purple']]
-  ]);
-
-  rootLevelNodes: string[] = ['Fruits', 'Vegetables'];
-
-  /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
-  }
-
-  getChildren(node: string): string[] | undefined {
-    return this.dataMap.get(node);
-  }
-
-  isExpandable(node: string): boolean {
-    return this.dataMap.has(node);
-  }
-}
-/**
- * File database, it can build a tree structured Json object from string.
- * Each node in Json object represents a file or a directory. For a file, it has filename and type.
- * For a directory, it has filename and children (a list of files or directories).
- * The input will be a json object string, and the output is a list of `FileNode` with nested
- * structure.
- */
-export class DynamicDataSource implements DataSource<DynamicFlatNode> {
-
-  dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
-
-  get data(): DynamicFlatNode[] { return this.dataChange.value; }
-  set data(value: DynamicFlatNode[]) {
-    this._treeControl.dataNodes = value;
-    this.dataChange.next(value);
-  }
-
-  constructor(private _treeControl: FlatTreeControl<DynamicFlatNode>,
-              private _database: DynamicDatabase) {}
-
-  connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
-    this._treeControl.expansionModel.changed.subscribe(change => {
-      if ((change as SelectionChange<DynamicFlatNode>).added ||
-        (change as SelectionChange<DynamicFlatNode>).removed) {
-        this.handleTreeControl(change as SelectionChange<DynamicFlatNode>);
-      }
-    });
-
-    return merge(collectionViewer.viewChange, this.dataChange).pipe(map(() => this.data));
-  }
-
-  disconnect(collectionViewer: CollectionViewer): void {}
-
-  /** Handle expand/collapse behaviors */
-  handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
-    if (change.added) {
-      change.added.forEach(node => this.toggleNode(node, true));
-    }
-    if (change.removed) {
-      change.removed.slice().reverse().forEach(node => this.toggleNode(node, false));
-    }
-  }
-
-  /**
-   * Toggle the node, remove from display list
-   */
-  toggleNode(node: DynamicFlatNode, expand: boolean) {
-    const children = this._database.getChildren(node.item);
-    const index = this.data.indexOf(node);
-    if (!children || index < 0) { // If no children, or cannot find the node, no op
-      return;
-    }
-
-    node.isLoading = true;
-
-    setTimeout(() => {
-      if (expand) {
-        const nodes = children.map(name =>
-          new DynamicFlatNode(name, node.level + 1, this._database.isExpandable(name)));
-        this.data.splice(index + 1, 0, ...nodes);
-      } else {
-        let count = 0;
-        for (let i = index + 1; i < this.data.length
-          && this.data[i].level > node.level; i++, count++) {}
-        this.data.splice(index + 1, count);
-      }
-
-      // notify the change
-      this.dataChange.next(this.data);
-      node.isLoading = false;
-    }, 1000);
-  }
-}
-
-/**
- * @title Tree with dynamic data
- */
 import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
@@ -136,7 +24,7 @@ export class VideoPageComponent implements OnInit {
   profile=new User();
   isLiked=false;
   private subscription: Subscription;
-  constructor(private activateRoute: ActivatedRoute,private service:VideoService, private commentservice:CommentService) {
+  constructor(private activateRoute: ActivatedRoute,private service:VideoService, private commentservice:CommentService,private router:Router) {
     const token = localStorage.getItem("token")
     if (token != null) {
       const jwtData = token.split('.')[1];
@@ -171,6 +59,10 @@ export class VideoPageComponent implements OnInit {
     this.service.isLiked(this.id as number,this.profile.name).subscribe((res:boolean)=>{
       this.isLiked=res;
     });
+    this.service.getAll().subscribe((res:any)=>{
+        this.videos = res
+        
+    })
   }
   like(){
     this.service.addLike(this.id as number,this.profile.name).subscribe((res:number)=>{
@@ -178,4 +70,13 @@ export class VideoPageComponent implements OnInit {
       this.isLiked=true;
     });
   }
+  videoRef(id:number){
+    this.router.navigate([`/video/${id}`]);
+    setTimeout(this.reload,1000)
+  }
+  reload()
+  {
+    window.location.reload()
+  }
+
 }
